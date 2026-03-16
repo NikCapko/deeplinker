ac9bc1ac957e78e4543677df3ed567723c1a7134
Nikolay <nik.capko@mail.ru>
2026-03-16 19:29:59 +0300

    fix buttons for add favorite and run

--- composeApp/src/jvmMain/kotlin/com/nikcapko/deeplinker/deeplinker/DeepLinkLauncherApp.kt
+++ composeApp/src/jvmMain/kotlin/com/nikcapko/deeplinker/deeplinker/DeepLinkLauncherApp.kt
@@ -1,13 +1,38 @@
 package com.nikcapko.deeplinker.deeplinker
 
+import PlayArrow
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +48,7 @@ import com.nikcapko.deeplinker.deeplinker.icons.Cross
 import com.nikcapko.deeplinker.deeplinker.icons.Edit
 import com.nikcapko.deeplinker.deeplinker.icons.Export
 import com.nikcapko.deeplinker.deeplinker.icons.Import
+import com.nikcapko.deeplinker.deeplinker.icons.Star
 import com.nikcapko.deeplinker.deeplinker.models.AdbDevice
 import com.nikcapko.deeplinker.deeplinker.models.DeepLinkEntry
 import com.nikcapko.deeplinker.deeplinker.utils.AdbExecutor
@@ -214,49 +240,50 @@ fun DeepLinkLauncherApp() {
             Spacer(modifier = Modifier.height(8.dp))
         }
 
-        // Поле ввода
-        OutlinedTextField(
-            value = inputUrl,
-            onValueChange = { inputUrl = it },
-            label = { Text("Введите deeplink") },
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            singleLine = true,
-            trailingIcon = {
-                if (inputUrl.isNotBlank()) {
-                    Icon(
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .pointerHoverIcon(PointerIcon.Hand)
-                            .clickable { inputUrl = "" }
-                            .padding(4.dp)
-                            .size(24.dp),
-                        imageVector = Cross,
-                        contentDescription = "clear input",
-                    )
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            // Поле ввода
+            OutlinedTextField(
+                value = inputUrl,
+                onValueChange = { inputUrl = it },
+                label = { Text("Введите deeplink") },
+                singleLine = true,
+                modifier = Modifier.weight(1f),
+                trailingIcon = {
+                    if (inputUrl.isNotBlank()) {
+                        Icon(
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .pointerHoverIcon(PointerIcon.Hand)
+                                .clickable { inputUrl = "" }
+                                .padding(4.dp)
+                                .size(24.dp),
+                            imageVector = Cross,
+                            contentDescription = "clear input",
+                        )
+                    }
                 }
-            }
-        )
+            )
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Кнопки
-        Row {
-            Button(
+            IconButton(
+                enabled = inputUrl.isNotBlank(),
                 onClick = {
                     if (inputUrl.isNotBlank()) {
                         urlToFavorite = inputUrl
                         showAddFavoriteDialog = true
                     }
                 },
-                modifier = Modifier.weight(0.5f),
-                enabled = inputUrl.isNotBlank(),
             ) {
-                Text("★")
+                Icon(
+                    imageVector = Star,
+                    contentDescription = "add to favorite",
+                )
             }
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Button(
+            IconButton(
+                enabled = inputUrl.isNotBlank() && selectedDevice != null,
                 onClick = {
                     if (inputUrl.isNotBlank() && selectedDevice != null) {
                         scope.launch {
@@ -281,10 +308,11 @@ fun DeepLinkLauncherApp() {
                         logOutput = "Выберите устройство!"
                     }
                 },
-                modifier = Modifier.weight(1f),
-                enabled = inputUrl.isNotBlank() && selectedDevice != null,
             ) {
-                Text("Запустить")
+                Icon(
+                    imageVector = PlayArrow,
+                    contentDescription = "run deeplink",
+                )
             }
         }
 
---
+++ composeApp/src/jvmMain/kotlin/com/nikcapko/deeplinker/deeplinker/icons/PlayArrow.kt
@@ -0,0 +1,39 @@
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val PlayArrow: ImageVector
+    get() {
+        if (_PlayArrow != null) {
+            return _PlayArrow!!
+        }
+        _PlayArrow = ImageVector.Builder(
+            name = "PlayArrow",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(fill = SolidColor(Color(0xFFE3E3E3))) {
+                moveTo(320f, 760f)
+                verticalLineToRelative(-560f)
+                lineToRelative(440f, 280f)
+                lineToRelative(-440f, 280f)
+                close()
+                moveTo(400f, 480f)
+                close()
+                moveTo(400f, 614f)
+                lineTo(610f, 480f)
+                lineTo(400f, 346f)
+                verticalLineToRelative(268f)
+                close()
+            }
+        }.build()
+
+        return _PlayArrow!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _PlayArrow: ImageVector? = null
